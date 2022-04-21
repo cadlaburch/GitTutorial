886e75cb3dc50c70b41c3325e3968c653cff5b0c
catalina <95720318+cadlaburch@users.noreply.github.com>
2022-04-21 10:57:19 -0700

    Uploading files

---
+++ Code/TestCode.R
@@ -0,0 +1,17 @@
+#hello Fishy people
+
+#Download US City Squirrel Data
+nyc_squirrels <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-29/nyc_squirrels.csv")
+
+#load libraries
+library(tidyverse)
+library(here) #setup
+library(sf) #vector data   
+library(spData) #datasets
+
+class(nyc_squirrels)
+
+ggplot(data = nyc_squirrels, aes(x=primary_fur_color))+
+  geom_histogram(stat = "count")
+
+
